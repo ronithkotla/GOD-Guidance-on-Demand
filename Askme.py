afb630093f5881488e6d634a7dd5a4d84869fddb
Kotla Ronith <165662580+ronithkotla@users.noreply.github.com>
2024-11-29 09:06:58 +0530

    Update Askme.py

--- Askme.py
+++ Askme.py
@@ -16,7 +16,7 @@ GROQ_API_KEY = "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"
 # Define the chatbot class
 class GroqChatbot:
     def __init__(self):
-        self.llm = ChatGroq(temperature=0, groq_api_key=GROQ_API_KEY, model_name="llama-3.1-70b-versatile")
+        self.llm = ChatGroq(temperature=0, groq_api_key=GROQ_API_KEY, model_name="llama-3.1-70b-versatile",proxies={...})
 
     def get_response(self, user_input):
         # Append the user's input to the conversation history
